1c04c59269c572bb33e4f25804abdfc6e60c5c22
”Sn1peRzGG” <afly367@gmail.com>
2026-06-08 09:56:25 +0300

    bug fixes

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 	app.use(cookieParser())
 
 	app.enableCors({
-		origin: 'http://localhost:3000',
+		origin: ['http://localhost:3000', 'https://foodzy-wheat.vercel.app/'],
 		credentials: true,
 	})
 
